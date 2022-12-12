89c3d14e6b1d03e342b05303afa89ee910ba23e9
RexGilang17 <giylankz@gmail.com>
2022-12-12 15:56:24 +0700

    first commit

---
+++ JavaProjectAkhir/e-ktpApp/src/main/java/com/model/AddLogin.java
@@ -0,0 +1,50 @@
+package model;
+
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb_user")
+public class AddLogin {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	//TB_KARTU_KELUARGA
+	private int id;
+	private String email;
+	private String nama;
+	private String password;
+	
+	public int getId() {
+		
+		return id;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getNama() {
+		return nama;
+	}
+
+	public void setNama(String nama) {
+		this.nama = nama;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+}
